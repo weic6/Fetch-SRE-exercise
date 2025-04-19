a64b4ac5a2258433293a628a834967f7e3fea2cf
weic6 <wei.rarr@gmail.com>
2025-04-19 04:42:30 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Fetch Take Home Exercise - Site Reliability Engineering
+
+This tools allows user to monitor >1000 endpoints health and report availability percentage (\*) every 15s.
+
+\* availability percentage = number of up / (number of down + number of up)
+
+## Quick start
+
+Prepare a yaml file containing endpoints (see sample.yaml). Then run the following commands to start the app
+
+```bash
+# skip these two commands, if you don't want to run it in a virtual python env
+python -m venv test_env
+source test_env/bin/activate
+
+# install dependency and start the app
+pip install -r requirements.txt
+python main.py sample.yaml
+```
+
+## Major changes to the code:
+
+1. use package `time` to calculate the endpoint response time, to determine of the endpoint is DOWN or UP.
+2. use `xxx.split(":")[0]` to ignore port numbers when determining domain.
+3. use `json.loads(body)` to convert JSON string (which is loaded from yaml file) into a Python dict in `check_health`, as `json=` in the request expects a Python `dict` object
+4. use `aiohttp` & `asyncio` to fire multiple HTTP request concurrently. The original `for endpoint in config:` implements serial http requests. Since we need to query every endpoint exactly every 15 seconds, we need make those request concurrent.
+5. update sleep time to `15 - endpoint response time`. This way we query every endpoint exactly every 15 mintutes.
+6. add `'GET'` in `method = endpoint.get('method', 'GET')` to make sure default is GET.
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import yaml
-import requests
 import time
+import json
 from collections import defaultdict
+import aiohttp
+import asyncio
 
 # Function to load configuration from the YAML file
 def load_config(file_path):
@@ -9,42 +11,58 @@ def load_config(file_path):
         return yaml.safe_load(file)
 
 # Function to perform health checks
-def check_health(endpoint):
+async def check_health(endpoint, domain_stats, session):
     url = endpoint['url']
-    method = endpoint.get('method')
+    method = endpoint.get('method', 'GET') # default is GET
     headers = endpoint.get('headers')
-    body = endpoint.get('body')
+    body = endpoint.get('body') # body is a string(!!), or None
+    body = json.loads(body) if body else None # convert JSON string into a Python dict
 
     try:
-        response = requests.request(method, url, headers=headers, json=body)
-        if 200 <= response.status_code < 300:
-            return "UP"
-        else:
-            return "DOWN"
-    except requests.RequestException:
-        return "DOWN"
+        time_start = time.time()        
+        async with session.request(method, url, headers=headers, json=body, timeout=5) as response:
+            time_taken = time.time() - time_start
+            # print(f"response time: {time_taken:.3f} s, status code: {response.status}")
+            if 200 <= response.status < 300 and time_taken <= 0.5:
+                res = "UP"
+            else:
+                res = "DOWN"
+    except asyncio.TimeoutError:
+        res = "DOWN"
+    
+    domain = url.split("//")[-1].split("/")[0].split(":")[0] # ignore port numbers when determining domain
+    domain_stats[domain]["total"] += 1
+    if res == "UP":
+        domain_stats[domain]["up"] += 1
 
 # Main function to monitor endpoints
-def monitor_endpoints(file_path):
+async def monitor_endpoints(file_path):
     config = load_config(file_path)
+    # num_endpoints = len(config)
     domain_stats = defaultdict(lambda: {"up": 0, "total": 0})
+    async with aiohttp.ClientSession() as session:
+        while True:
+            tasks = []
+            time_start = time.time()     
+            # print(f"---checking {num_endpoints} endpoints health at {time.strftime('%Y/%m/%d %H:%M:%S')}---") 
+            
+            for endpoint in config:
+                task = asyncio.create_task(coro = check_health(endpoint, domain_stats, session))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
 
-    while True:
-        for endpoint in config:
-            domain = endpoint["url"].split("//")[-1].split("/")[0]
-            result = check_health(endpoint)
+            # Log cumulative availability percentages
+            for domain, stats in domain_stats.items():
+                availability = round(100 * stats["up"] / stats["total"])
+                print(f"{domain} has {availability}% availability percentage")
 
-            domain_stats[domain]["total"] += 1
-            if result == "UP":
-                domain_stats[domain]["up"] += 1
-
-        # Log cumulative availability percentages
-        for domain, stats in domain_stats.items():
-            availability = round(100 * stats["up"] / stats["total"])
-            print(f"{domain} has {availability}% availability percentage")
-
-        print("---")
-        time.sleep(15)
+            print("---")
+            
+            cycle_time = 15 # health check runs every 15 seconds
+            time_taken = time.time() - time_start
+            time_sleep = cycle_time - time_taken
+            if time_sleep > 0:
+                await asyncio.sleep(time_sleep)
 
 # Entry point of the program
 if __name__ == "__main__":
@@ -56,6 +74,6 @@ if __name__ == "__main__":
 
     config_file = sys.argv[1]
     try:
-        monitor_endpoints(config_file)
+        asyncio.run(monitor_endpoints(config_file))
     except KeyboardInterrupt:
         print("\nMonitoring stopped by user.")
